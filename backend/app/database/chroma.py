d9352d06fdbf34ba61ce2ac979231605e25993c1
juven0 <bocquelin@gmail.com>
2026-01-28 21:54:35 +0300

    add retrive system

---
+++ backend/app/database/chroma.py
@@ -0,0 +1,13 @@
+import chromadb
+from langchain_vectrorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings 
+
+def query_chroma_db(query: str):
+    client = chromadb.Client()
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
+    
+    vector_store = Chroma(client=client, embedding_function=embeddings)
+    
+    results = vector_store.similarity_search(query)
+    
+    return results
